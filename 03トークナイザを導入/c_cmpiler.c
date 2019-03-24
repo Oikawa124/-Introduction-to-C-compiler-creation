22a927f93a4d9b0fa96d6a72827cbcefe68b881c
devel <oiku1515@gmail.com>
2019-03-24 23:57:56 +0900

    [add] tokenize()とunit testを追加

---
+++ 03トークナイザを導入/c_cmpiler.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+#include <mem.h>
+
+// トークンの型を表す値
+enum {
+    TK_NUM = 256, // 整数トークン
+    TK_EOF,       // 入力の終わりを表すトークン EOF
+};
+
+// トークンの型
+typedef struct {
+    int ty;  // トークンの型
+    int val; // tyがTK_NUMの場合、数値をいれる
+    char *input; // トークン文字列（エラーメッセージ）
+} Token;
+
+
+// トークナイズした結果のトークン列はこの配列に保存する
+// 100個以上のトークンは来ないものとする
+Token tokens[100];
+
+
+void tokenize(char *p){
+    int i = 0;
+
+    // 空白文字スキップ
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
+            p++; // ポインタを進める
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+
+        fprintf(stderr, "fail %s\n", p);
+        exit(1);
+    }
+}
+
+
+
+//static int streq(char *s1, char *s2) { return 0 == strcmp(s1, s2); }
+
+static void test_tokenize_1(){
+
+    // SetUp
+    char *input = "1 + 1";
+
+    // Exercise
+    tokenize(input);
+
+    // Verify
+    assert(tokens[0].ty == TK_NUM);
+    assert(tokens[0].val == 1);
+
+    assert(tokens[1].ty == '+');
+
+
+    assert(tokens[2].ty == TK_NUM);
+    assert(tokens[2].val == 1);
+
+}
+
+static void unit_tests(){
+    test_tokenize_1();
+}
+
+
+int main(int argc, char **argv) {
+
+    // ユニットテスト
+    unit_tests();
+
+    if (argc != 2) {
+        fprintf(stderr, "invalid arg");
+        return 1;
+    }
+
+    // トークナイズする
+    tokenize(argv[1]);
+
+
+    // アセンブリの前半部分
+//    printf(".intel_syntax noprefix\n");
+//    printf(".global main\n");
+//    printf("main:\n");
+//
+//    printf("  mov rax, %d\n", atoi(argv[1]));
+//    printf("  ret\n");
+    return 0;
+}
