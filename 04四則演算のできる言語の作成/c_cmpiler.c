8432fef8df92812fd848c1db822cb2b82e183910
devel <oiku1515@gmail.com>
2019-03-25 14:08:15 +0900

    first commit

---
+++ 04四則演算のできる言語の作成/c_cmpiler.c
@@ -0,0 +1,278 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+
+
+
+
+
+// トークンの型を表す値
+enum {
+    TK_NUM = 256, // 整数トークン
+    TK_EOF,       // 入力の終わりを表すトークン EOF
+};
+
+// トークンの型
+typedef struct {
+    int ty;  // トークンの型
+    int val; // tyがTK_NUMの場合、数値をいれる
+    char *input; // トークン文字列（エラーメッセージ）
+} Token;
+
+
+// トークナイズした結果のトークン列はこの配列に保存する
+// 100個以上のトークンは来ないものとする
+Token tokens[100];
+int pos=0;
+
+
+void tokenize(char *p){
+    int i = 0;
+
+    // 空白文字スキップ
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
+            p++; // ポインタを進める
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+
+        fprintf(stderr, "fail %s\n", p);
+        exit(1);
+    }
+
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
+}
+
+// エラーを報告する関数
+void error(int i){
+    fprintf(stderr, "error: %s\n", tokens[i].input);
+    exit(1);
+}
+
+
+
+// 抽象構文木のノードを作成
+
+enum {
+    NO_MUM =  256, // 整数ノードの型
+};
+
+typedef struct Node {
+    int ty;           // 演算子かND_NUM
+    struct Node *lhs; // 左辺  left hand side
+    struct Node *rhs; // 右辺  right hand side
+    int val;          // ty がND_NUMの場合のみ使う
+}Node;
+
+// 新しいNode作成
+
+Node *new_node(int ty, Node *lhs, Node *rhs) { // 右辺と左辺を受け取る2項演算子
+
+    Node *node = malloc(sizeof(Node));
+
+    node->ty = ty;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val){
+    Node *node = malloc(sizeof(Node));
+
+    node->ty = NO_MUM;
+    node->val = val;
+
+    return node;
+}
+
+int consume(int ty) {
+    if (tokens[pos].ty != ty) {
+        return 0;
+    }
+    pos++;
+    return 1;
+}
+
+
+
+// 関数とデータ型を使ってパーサーを書く
+
+Node *add();
+
+
+Node *term(){
+
+    if (consume('(')) {
+        Node *node = add();
+
+        if (!consume(')')) {
+            error(pos);
+        }
+        return node;
+    }
+
+    if (tokens[pos].ty == TK_NUM) {
+        return new_node_num(tokens[pos++].val);
+    }
+
+    error(pos);
+}
+
+
+Node *mul(){
+    Node *node = term();
+
+    for(;;){
+        if (consume('*')) {
+
+            node = new_node('*', node, mul());
+
+        } else if (consume('/')) {
+
+            node = new_node('/', node, mul());
+
+        } else {
+
+            return node;
+        }
+    }
+}
+
+
+
+Node *add(){
+    Node *node = mul();
+
+    for(;;){
+        if (consume('+')) {
+
+            node = new_node('+', node, mul());
+
+        } else if (consume('-')) {
+
+            node = new_node('-', node, mul());
+
+        } else {
+
+            return node;
+        }
+    }
+
+}
+
+
+
+
+
+
+//static int streq(char *s1, char *s2) { return 0 == strcmp(s1, s2); }
+
+static void test_tokenize_1(){
+
+    // SetUp
+    char *input = "1 + 1";
+
+    // Exercise
+    tokenize(input);
+
+    // Verify
+    assert(tokens[0].ty == TK_NUM);
+    assert(tokens[0].val == 1);
+
+    assert(tokens[1].ty == '+');
+
+    assert(tokens[2].ty == TK_NUM);
+    assert(tokens[2].val == 1);
+
+    assert(tokens[3].ty == TK_EOF);
+}
+
+static void unit_tests(){
+    test_tokenize_1();
+}
+
+
+int main(int argc, char **argv) {
+
+    // ユニットテスト
+    unit_tests();
+
+    if (argc != 2) {
+        fprintf(stderr, "invalid arg");
+        return 1;
+    }
+
+    // トークナイズする
+    tokenize(argv[1]);
+
+
+    // アセンブリの前半部分
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+
+    // 式の最初は数字である必要がある
+    // 最初のmov命令を出力
+
+    if (tokens[0].ty != TK_NUM) {
+        error(0);
+    }
+
+    printf("  mov rax, %d\n", tokens[0].val);
+
+    // アセンブリを出力
+    int i = 1;
+    while (tokens[i].ty != TK_EOF) {
+
+        if (tokens[i].ty == '+') {
+
+            i++; // "+"の次に移動。　+<数字>
+
+            if (tokens[i].ty != TK_NUM) {
+                error(i);
+            }
+
+            printf("  add rax, %d\n", tokens[i].val);
+
+            i++; // 次のトークンに移動
+            continue;
+        }
+
+        if (tokens[i].ty == '-') {
+
+            i++; // "-"の次に移動。　+<数字>
+
+            if (tokens[i].ty != TK_NUM) {
+                error(i);
+            }
+
+            printf("  sub rax, %d\n", tokens[i].val);
+
+            i++; // 次のトークンに移動
+            continue;
+        }
+
+        error(i);
+    }
+
+    printf("  ret\n");
+    return 0;
+}
